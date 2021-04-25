546b226d3c0b31e4eaa61b97b52da4bec43ba7d2
sujoy-coder <ask2sujoy@gmail.com>
2021-04-25 13:32:57 +0530

    make changes settings debug

--- src/settings.py
+++ src/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', SECRET_KEY_TEST)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = (os.environ.get('DEBUG'), False)
+DEBUG = int(os.environ.get('DEBUG')=="True")
 
 ALLOWED_HOSTS = []
 
